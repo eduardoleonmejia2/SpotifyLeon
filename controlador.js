6b95de2df539dad1337b11aabd2fa89a135685fb
eduardo leon <eduardoleonmejia2@gmail.com>
2020-07-10 15:07:35 -0500

    se realizaron cambios

--- controlador.js
+++ controlador.js
@@ -1,35 +1,49 @@
-//este sera el consumo del servicio de spotify para traer conciones 
+//Consumo del servicio de spotyfy para
+//traer canciones de un artista
 
 
-//1. Definir la url. 
+//DEFINIR LA VARIABLE PARA RECIBIR LA ETIQUETA DE IMG1
+//let imagen1 = document.getElementById("imagen1");
 
-let url="https://api.spotify.com/v1/artists/4rTv3Ejc7hKMtmoBOK1B4T/top-tracks?country=ES";
+//1. Definir la URL del servicio
+let url = "https://api.spotify.com/v1/artists/4rTv3Ejc7hKMtmoBOK1B4T/top-tracks?country=ES";
 
-//2. Definir el token o permiso de acceso. 
+//2. Definir el token o permiso de acceso
+let token = "Bearer BQCy5ORNesfoXGLUJdh4zlw2BOlVcbK4IjXOC2l8nWpQltBu9yPJTawNgK7cwWH9sKEdSZ7B__VIJ7wZJbwJ8W1rwcwNK6aS9mM62_0nQ7-JWraGy70s0AjCijLJ9iEjegZm_nwHfT3hOdQ8";
 
-let token="Bearer BQCX6bfKxd3sfAz_wKIBESMzX9_1VP7hh7jMfBmR7qjHhTULBKpnTwLwpGkmGqYVweZfC__s_5Ufn32P4835L3F7sfh0IzxLE7VYBMNQvUbTnJ-czW0LRoFwl0iKLHGCJv-A4LCfrIrICvSXkDVy-L1S_FftNPE";
+let token2 = "Bearer BQCOcEOsDkuEwkfiyRuIaKnTQEZxNeKRY-zQb5FT1CFmtT4YqslbAIto0LixUycv-hEQVb-FAjqJsxv6BB17A2t89CDsck0BMISOqT6Ycurn0JZubvg0ETJNpJl7OdFXHXXS6CWMejSdK_Tjl7pWDjNeeEoa4dQ";
+    //3.Definir Objeto AJAX
+let objetoAJAX = new XMLHttpRequest();
 
-//3. Definir objeto AJAX.
+//4. Abrir la conexion
+objetoAJAX.open('GET', url, true);
 
-let objAJAX= new XMLHttpRequest(); 
+//5. crear encabezado de conexion
+objetoAJAX.setRequestHeader('Authorization', token2);
 
-//4. Abrir la conexion.
-
-objAJAX.open('GET', url, true);
-
-//5. Crear en cabezado de conexcion.
-
-objAJAX.setRequestHeader8('Authorization',token);
-
-//6. Cargarlos datos del sevircio.
-objAJAX.onload = function() {
-
+//6. cargamos los datos del servicio
+objetoAJAX.onload = function() {
     let respuesta = JSON.parse(this.responseText);
     console.log(respuesta);
     console.log(respuesta.tracks);
     console.log(respuesta.tracks[0]);
     console.log(respuesta.tracks[0].album);
+    console.log(respuesta.tracks[0].album.images);
+    console.log(respuesta.tracks[0].album.images[0]);
+    console.log(respuesta.tracks[0].album.images[0].url);
+    console.log(respuesta.tracks[0].album.name);
+    console.log(respuesta.tracks[0].preview_url);
+
+
+
+    let tutilo = document.getElementById("titulo");
+    titulo.textContent = respuesta.tracks[0].album.name;
+
+    let audio = document.getElementById("audio");
+    audio.src = respuesta.tracks[0].preview_url;
 }
 
-//Enviamos la peticion
-objAJAX.send(); 
+
+
+//7. enviamos la peticion
+objetoAJAX.send();
